780beffd70f4d1571982bce664d36b2eda6aac37
Tom <p.tommy93@gmail.com>
2017-06-21 23:41:06 +0200

    Add border to favourite images

--- dist/ng-image-gallery.js
+++ dist/ng-image-gallery.js
@@ -78,7 +78,7 @@
 							// Thumbnails container
 							//  Hide for inline gallery
 							'<div ng-if="thumbnails && !inline" class="ng-image-gallery-thumbnails">'+
-								'<div class="thumb" ng-repeat="image in images track by $index" ng-click="methods.open($index);" ng-style="{ \'background-image\': \'url(\' + (image.thumbUrl || image.url) + \')\' }"  ng-attr-title="{{image.title || undefined}}"></div>'+
+								'<div class="thumb" ng-class="{\'thumb-favourite\': !hideStarButton && image.favourite}" ng-repeat="image in images track by $index" ng-click="methods.open($index);" ng-style="{ \'background-image\': \'url(\' + (image.thumbUrl || image.url) + \')\' }"  ng-attr-title="{{image.title || undefined}}"></div>'+
 							'</div>'+
 
 							// Modal container
